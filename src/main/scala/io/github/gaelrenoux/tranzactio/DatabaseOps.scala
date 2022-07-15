26ddf6db62a424ab3737ed5d0d1a7ea6dad75765
Sergey Rublev <sergey.rublev@dataspike.io>
2022-07-16 01:27:30 +0300

    Scala 3 migration

--- src/main/scala/io/github/gaelrenoux/tranzactio/DatabaseOps.scala
+++ src/main/scala/io/github/gaelrenoux/tranzactio/DatabaseOps.scala
@@ -22,13 +22,6 @@ trait DatabaseOps[Connection, R0] {
       commitOnFailure: => Boolean = false
   )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, Either[DbException, E], A]
 
-  @deprecated("Use transaction instead.", since = "0.4.0")
-  final def transactionR[R, E, A](
-      zio: => ZIO[Connection with R, E, A],
-      commitOnFailure: => Boolean = false
-  )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, Either[DbException, E], A] =
-    transaction[R, E, A](zio, commitOnFailure)
-
   /** As `transaction`, but exceptions are simply widened to a common failure type. The resulting failure type is a
    * superclass of both DbException and the error type of the inital ZIO. */
   final def transactionOrWiden[R, E >: DbException, A](
@@ -37,26 +30,12 @@ trait DatabaseOps[Connection, R0] {
   )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, E, A] =
     transaction[R, E, A](zio, commitOnFailure).mapError(_.fold(identity, identity))
 
-  @deprecated("Use transactionOrWiden instead.", since = "4.0.0")
-  final def transactionOrWidenR[R, E >: DbException, A](
-      zio: => ZIO[Connection with R, E, A],
-      commitOnFailure: => Boolean = false
-  )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, E, A] =
-    transactionOrWiden[R, E, A](zio, commitOnFailure)
-
   /** As `transaction`, but errors when handling the connections are treated as defects instead of failures. */
   final def transactionOrDie[R, E, A](
       zio: => ZIO[Connection with R, E, A],
       commitOnFailure: => Boolean = false
   )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, E, A] =
     transaction[R, E, A](zio, commitOnFailure).flatMapError(dieOnLeft)
-
-  @deprecated("Use transactionOrDie instead.", since = "4.0.0")
-  final def transactionOrDieR[R, E, A](
-      zio: => ZIO[Connection with R, E, A],
-      commitOnFailure: => Boolean = false
-  )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, E, A] =
-    transactionOrDie[R, E, A](zio, commitOnFailure)
 
   /** Provides that ZIO with a Connection. All DB action in the ZIO will be auto-committed. Failures in the initial
    * ZIO will be wrapped in a Right in the error case of the resulting ZIO, with connection errors resulting in a
@@ -68,12 +47,6 @@ trait DatabaseOps[Connection, R0] {
       zio: => ZIO[Connection with R, E, A]
   )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, Either[DbException, E], A]
 
-  @deprecated("Use autoCommit instead.", since = "4.0.0")
-  final def autoCommitR[R, E, A](
-      zio: => ZIO[Connection with R, E, A]
-  )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, Either[DbException, E], A] =
-    autoCommit[R, E, A](zio)
-
   /** As `autoCommit`, but exceptions are simply widened to a common failure type. The resulting failure type is a
    * superclass of both DbException and the error type of the inital ZIO. */
   final def autoCommitOrWiden[R, E >: DbException, A](
@@ -81,24 +54,11 @@ trait DatabaseOps[Connection, R0] {
   )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, E, A] =
     autoCommit[R, E, A](zio).mapError(_.fold(identity, identity))
 
-  @deprecated("Use autoCommitOrWiden instead.", since = "4.0.0")
-  final def autoCommitOrWidenR[R, E >: DbException, A](
-      zio: => ZIO[Connection with R, E, A]
-  )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, E, A] =
-    autoCommitOrWiden[R, E, A](zio)
-
   /** As `autoCommit`, but errors when handling the connections are treated as defects instead of failures. */
   final def autoCommitOrDie[R, E, A](
       zio: => ZIO[Connection with R, E, A]
   )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, E, A] =
     autoCommit[R, E, A](zio).flatMapError(dieOnLeft)
-
-  @deprecated("Use autoCommitOrDie instead.", since = "4.0.0")
-  final def autoCommitOrDieR[R, E, A](
-      zio: => ZIO[Connection with R, E, A]
-  )(implicit errorStrategies: ErrorStrategiesRef = ErrorStrategies.Parent, trace: Trace): ZIO[R with R0, E, A] =
-    autoCommitOrDie[R, E, A](zio)
-
 }
 
 object DatabaseOps {
